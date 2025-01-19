5fdfc2054143b68825acb6afd2a2d855825177c4
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-19 20:16:03 +0530

    html and css part completed

---
+++ codes.js
@@ -0,0 +1,15 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    IN: "INR",
+    US: "USD",
+}
